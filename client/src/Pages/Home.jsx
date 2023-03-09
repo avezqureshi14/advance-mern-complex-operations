6a57a6e4c3826740d8cd838ab505d717f59e9479
ubaidubai <avez3npqureshi@gmail.com>
2023-03-09 13:12:14 +0530

    BUILD ENVIRONMENT READY

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import FormCard from '../components/FormCard'
-import Posts from '../components/Posts/Posts'
-
-const Home = ({currentId,setCurrentId}) => {
+import { Paper } from "@material-ui/core";
+import React from "react";
+import FormCard from "../components/FormCard";
+import Paginate from "../components/Pagination";
+import Posts from "../components/Posts/Posts";
+import useStyles from './styles';
+const Home = ({ currentId, setCurrentId }) => {
+  const classes = useStyles();
   return (
     <>
-    <div className="wrapper">
-            <Posts currentId={currentId} setCurrentId={setCurrentId} />
-            <FormCard currentId={currentId} setCurrentId={setCurrentId} />
-    </div>
+      <div className="wrapper">
+        <div className="posts">
+          <Posts currentId={currentId} setCurrentId={setCurrentId} />
+        </div>
+        <div className="form">
+          <FormCard currentId={currentId} setCurrentId={setCurrentId} />
+          <Paper className={classes.pagination} elevation={6} >
+          <Paginate />
+          </Paper>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
